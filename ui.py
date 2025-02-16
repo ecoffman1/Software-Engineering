a52c402b5a96aa1e98ba12b62c223705ab1554a0
ecoffman1 <ejc012@uark.edu>
2025-02-16 03:19:23 -0600

    added prompt for codename

--- ui.py
+++ ui.py
@@ -4,7 +4,7 @@ from UDP.changeSettings import changeSettings
 from database import Database
 
 def query(id):
-    if(id.isnumeric()):
+    if(id == 1):
         return "Player1"
     return ""
 
@@ -24,6 +24,7 @@ class Splash(ctk.CTkToplevel):
 class TeamFrame(ctk.CTkFrame):
     def __init__(self, master, title):
         super().__init__(master)
+        self.validating = None
 
         if title == "Red":
             self.color = "#591717"
@@ -47,31 +48,70 @@ class TeamFrame(ctk.CTkFrame):
             row[0] = ctk.CTkEntry(self,width = 100, height=30,placeholder_text="",corner_radius=0, fg_color="White", text_color="Black")
             row[0].grid(row=i+1, column=1,padx=(5,0), pady=5, sticky="nw")
             row[0].bind("<FocusOut>", self.validate)
-            row[0].bind("<Return>", self.validate)
             row[1] = ctk.CTkLabel(self, width = 200, height=30, fg_color="White", text_color="Black",text="")
             row[1].grid(row=i+1, column=2,padx=(0,10), pady=5, sticky="ne")
             self.widgets.append(row)
 
     def validate(self, event):
         box = event.widget
-        response = self.master.store_query(box.get(),self.bg_color)
-        if(len(response) > 0):
-            for i in range(len(self.widgets)):
-                if(self.widgets[i][0].get() == box.get()):
-                    self.widgets[i][1].configure(text=response)
-        else:
-            box.delete(0, 999)
+        id = box.get()
+        if(not id.isnumeric()):
+            row = self.findRow(id)
+            self.widgets[row][1].configure(text="")
+            self.master.clear(row,self.bg_color)
+            box.delete(0,999)
+            return
+        
+        response = self.master.store_query(id,self.bg_color)
+
+        if(response == ""):
+            return
+        
+        for i in range(len(self.widgets)):
+            if(self.widgets[i][0].get() == id):
+                self.widgets[i][1].configure(text=response)
+        return "break"
+
+    def lock(self):
+        for i in range(len(self.widgets)):
+            self.widgets[i][0].configure(state="disabled")
+    
+    def unlock(self):
+        for i in range(len(self.widgets)):
+            self.widgets[i][0].configure(state="normal")
+    
+    def findRow(self, id):
+        for i in range(len(self.widgets)):
+            if(self.widgets[i][0].get() == id):
+                return i
+        return -1
+    
+    def set(self, index, value):
+        self.widgets[index][1].configure(text=value)
 
 class App(ctk.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.withdraw()
-        self.player_list_g = []
-        self.player_list_r = []
+        self.player_list_g = [[None,None] for i in range(15)]
+        self.player_list_r = [[None,None] for i in range(15)]
+        self.popup = None
+        
+        self.curid = None
 
         # Initialize database connection
         self.db = Database()
         self.db.connect()
+
+    def lock(self):
+        self.button.configure(state="disabled")
+        self.red.lock()
+        self.green.lock()
+    
+    def unlock(self):
+        self.button.configure(state="normal")
+        self.red.unlock()
+        self.green.unlock()
       
     def splash(self):
         self.splash = Splash(self)
@@ -97,15 +137,17 @@ class App(ctk.CTk):
 
     def on_button_click(self):
         changeSettings()
+    
+
 
     def store_query(self,id,color):
+        #TODO Joey this should be a query method called from self.db that returns the codename if there  is one or an empty string if there isnt
         response = query(id)
         if(len(response) > 0):
             player = (id, response)
 
-            #TODO: Prevent the player data being inserted multiple times.
-            # Insert player into the database
-            self.db.add_player(id, response)
+            #TODO Joey I keep getting an error that the cursor is none for the add_player method fixed issue of being called twice
+            #self.db.add_player(id, response)
 
             if color == "Red":
                 self.player_list_r.append(player)
@@ -113,12 +155,69 @@ class App(ctk.CTk):
                 self.player_list_g.append(player)
             return response
         else:
+            self.curid = id
             self.askForCodename()
             return ""
+        
 
+    def clear(self, row, color):
+        if(color == "Red"):
+            self.player_list_r[row] = [None,None]
+        else:
+            self.player_list_g[row] = [None,None]
+
+    def store_id(self,id):
+        red_index = self.red.findRow(id)
+        green_index = self.green.findRow(id)
+
+        if(red_index > -1):
+            self.player_list_r[red_index][0] = id
+        if(green_index > -1):
+            self.player_list_g[green_index][0] = id
     
+    def store_codename(self,id, codename):
+        red_index = self.red.findRow(id)
+        green_index = self.green.findRow(id)
+
+        if(red_index > -1):
+            self.player_list_r[red_index][0] = id
+            self.player_list_r[red_index][1] = codename
+            self.red.set(red_index, codename)
+        if(green_index > -1):
+            self.player_list_g[green_index][0] = id
+            self.player_list_g[green_index][1] = codename
+            self.green.set(green_index, codename)
+
     def askForCodename(self):
-        pass
+        if(self.popup != None):
+            return
+        
+        #Lock input for app
+        self.lock()
+
+        #Frame for popup
+        self.popup = ctk.CTkFrame(self)
+        self.popup.place(relx = .5, rely = .5, anchor = "center")
+        
+        #Instructions
+        self.popup_instructions = ctk.CTkLabel(self.popup,width = 10, text="No codename was found for this id,\n please enter one")
+        self.popup_instructions.grid(row = 0, column = 0, padx = 10, pady = 10)
+
+        #Entrybox for codename
+        self.popup_entry = ctk.CTkEntry(self.popup,width = 100, height=30,placeholder_text="",corner_radius=0, fg_color="White", text_color="Black")
+        self.popup_entry.bind("<Return>", self.codenameRecieved)
+        self.popup_entry.grid(row = 1, column = 0, pady=10)
+        self.popup_entry.focus_force()
+        
+    def codenameRecieved(self, event):
+        codename = event.widget.get()
+        self.store_codename(self.curid,codename)
+        print(self.player_list_g)
+        print(self.player_list_r)
+        #TODO Joey we need to store the self.curid and codename in the database
+        self.unlock()
+        self.popup.destroy()
+        self.popup = None
 
 
 

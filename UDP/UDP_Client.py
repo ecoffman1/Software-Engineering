fa759262158d63e4942fd877300ffb0ca51d33bc
funcRandy <rnallnae@gmail.com>
2025-02-15 23:00:02 -0600

    Removed socket closure so the client can continue broadcasting. Took socket outside of function so it doesn't try to create a new socket every time it is called to send a message. Added message argument for sending equipment id.

--- UDP/UDP_Client.py
+++ UDP/UDP_Client.py
@@ -9,20 +9,17 @@ with open("config.json", "r") as config_file:
 # get ip address and port from dictionary
 udp_ip = config["udp_ip"]
 broadcastPort = config["broadcastPort"]
-clientMessage = "Hello server, I'm the client!"
 
-def client():
+# create a udp socket for transmitting
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # create a udp socket for transmitting
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def client(message):
 
     # send message to the server
-    sock.sendto(clientMessage.encode(), (udp_ip, broadcastPort))
+    sock.sendto(message.encode(), (udp_ip, broadcastPort))
 
     print(f"Message sent to: {udp_ip}:{broadcastPort}")
 
     # response from server
     response, _ = sock.recvfrom(1024)
     print(f"Response: {response.decode()}")
-
-    sock.close()
